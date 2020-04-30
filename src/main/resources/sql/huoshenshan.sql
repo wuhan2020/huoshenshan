46a90274dc63f136d9c6cc5b500d1c88bdd12245
Xin Wang <xin.victorwang@gmail.com>
2020-04-30 17:02:38 +0800

    Update huoshenshan.sql

--- src/main/resources/sql/huoshenshan.sql
+++ src/main/resources/sql/huoshenshan.sql
@@ -1,42 +1,19 @@
-/*
-Navicat MySQL Data Transfer
-
-Source Server         : 
-Source Server Version : 50647
-Source Host           : 
-Source Database       : huoshenshan
-
-Target Server Type    : MYSQL
-Target Server Version : 50647
-File Encoding         : 65001
-
-Date: 2020-04-30 16:50:36
-*/
-
+ 
 SET FOREIGN_KEY_CHECKS=0;
-
--- ----------------------------
--- Table structure for user
--- ----------------------------
+ 
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL,
-  `phoneNumber` varchar(255) DEFAULT NULL,
+  `phone_number` varchar(255) DEFAULT NULL,
   `sex` int(11) DEFAULT NULL,
   `birthday` datetime DEFAULT NULL,
   `profession` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- ----------------------------
--- Records of user
--- ----------------------------
-
--- ----------------------------
--- Table structure for vedio
--- ----------------------------
+ 
 DROP TABLE IF EXISTS `vedio`;
 CREATE TABLE `vedio` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
@@ -44,13 +21,10 @@ CREATE TABLE `vedio` (
   `fileExtension` varchar(255) DEFAULT NULL,
   `publisher` varchar(255) DEFAULT NULL,
   `description` varchar(255) DEFAULT NULL,
-  `createdTime` datetime DEFAULT NULL,
-  `updatedTime` datetime DEFAULT NULL,
-  `hackathonId` int(11) DEFAULT NULL,
-  `hackathonStage` int(11) DEFAULT NULL,
+  `created_time` datetime DEFAULT NULL,
+  `updated_time` datetime DEFAULT NULL,
+  `hackathon_id` int(11) DEFAULT NULL,
+  `hackathon_stage` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of vedio
--- ----------------------------
+ 
