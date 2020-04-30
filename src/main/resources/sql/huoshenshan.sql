5f7e39c75b680f0da9662c9dd9715ece9ce96f7d
JGX020 <1449458000@qq.com>
2020-04-30 17:03:02 +0800

    sql (#10)

--- src/main/resources/sql/huoshenshan.sql
+++ src/main/resources/sql/huoshenshan.sql
@@ -1,6 +1,30 @@
-# user table
+ 
+SET FOREIGN_KEY_CHECKS=0;
+ 
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `phone_number` varchar(255) DEFAULT NULL,
+  `sex` int(11) DEFAULT NULL,
+  `birthday` datetime DEFAULT NULL,
+  `profession` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-
-# video table
-
-
+ 
+DROP TABLE IF EXISTS `vedio`;
+CREATE TABLE `vedio` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `fileName` varchar(255) DEFAULT NULL,
+  `fileExtension` varchar(255) DEFAULT NULL,
+  `publisher` varchar(255) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `created_time` datetime DEFAULT NULL,
+  `updated_time` datetime DEFAULT NULL,
+  `hackathon_id` int(11) DEFAULT NULL,
+  `hackathon_stage` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+ 
